1b31274defd6f3785b569d4a3bac4f3807c33840
Joseph Cheel Nakashima <josephcheel@MacBook-Air-de-Joseph.local>
2023-10-18 02:22:17 +0200

    more fixes on variables

--- src/variables/ft_parsevar.c
+++ src/variables/ft_parsevar.c
@@ -1,29 +1,40 @@
 #include "../../inc/minishell.h"
 
-char *ft_get_variable(char *variable)
+char *ft_get_export_id(char *variable)
 {
 	char **split;
 
 	if (variable[0] == '$')
 		variable++;
 	split = ft_split(variable, ' ');
-	return (split[0]);
+	// printf("split: %s\n", split[0]);
+	return (ft_strtrim(split[0], "\""));
 }
 
 char *ft_parse_variables(t_minishell *data)
 {
+	char *result;
 	char *variable;
 	t_env *temp;
 
+
 	variable = ft_strchr(data->raw_cmd, '$');
-	if (variable)
+	if (!ft_has_valid_quotes(variable) && ft_strchr(variable, '\'') && (ft_strchr(variable, '\'') < ft_strchr(variable, '\"') || !ft_strchr(variable, '\"')))
+		return (data->raw_cmd);
+	if (variable && ft_strlen(variable) > 1)
 	{
-		variable = ft_get_variable(variable);
+		variable = ft_get_export_id(variable);
 		if (ft_isvariable(data->lstenv, variable))
 		{
 			temp = ft_find_id(data->lstenv, variable);
-			printf("replaced string %s\n", ft_replace_string(data->raw_cmd, temp->id, temp->value));			
+			result = ft_replace_string(data->raw_cmd, temp->id, temp->value);
+			return (result);		
+		}
+		else
+		{	
+			result = ft_replace_string(data->raw_cmd, variable, "");
+			return (result);
 		}
 	}
-	return (NULL);
+	return (data->raw_cmd);
 }
