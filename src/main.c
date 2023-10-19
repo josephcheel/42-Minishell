f0ce8475f3fb373d0dff7fbe9cfdb84636548531
Joseph Cheel nakashima <jcheel-n@car4s3.42barcelona.com>
2023-10-19 19:25:58 +0200

    merging

--- src/main.c
+++ src/main.c
@@ -1,31 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcheel-n <jcheel-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 19:00:06 by jcheel-n          #+#    #+#             */
+/*   Updated: 2023/10/19 19:19:48 by jcheel-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/minishell.h"
 
-
-int init_term(t_minishell *data)
+int	init_minishell(t_minishell *data, char **env)
 {
-	//get terminal settings
-	if (tcgetattr(STDIN_FILENO, &data->new_settings) != 0) 
-	{
-		perror("tcgetattr");
+	if (init_env(data, env))
 		return (1);
-	}
- 	data->old_settings = data->new_settings;
-	data->new_settings.c_lflag &= ~ECHOCTL;
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &data->new_settings) != 0) {
-        perror("tcsetattr");
-        return 1;
-    }
+	if (init_term(data))
+		return (1);
+	if (init_term_fd(data))
+		return (1);
+	data->status = 0;
 	return (0);
 }
 
-int	restore_term(t_minishell *data)
+int	finish_minishell(t_minishell *data)
 {
-	//restore originial terminal settings
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &data->old_settings) != 0) {
-        perror("tcsetattr");
-        return 1;
-    }
+	close_term_fd(data);
+	restore_term(data);
 	return (0);
+}
+
+int	ft_ctrl_d(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(2, "exit\n", 6);
+	exit (EXIT_SUCCESS);
 }
 
 int	main(int ac, char **av, char **env)
@@ -34,43 +44,19 @@ int	main(int ac, char **av, char **env)
 
 	if (ac != 1 && !av)
 		return (1);
-	init_env(&data, env);
-	init_term(&data);
-	
-	//for ctrl + c
-	/*	if (signal(SIGINT, signal_handler) == SIG_ERR)
-			{
-			perror("signal");
-			return(1);
-			}
-	*/
-	
+	if (init_minishell(&data, env))
+		;
 	while (1)
 	{
-		data.std_in = dup(STDIN_FILENO);
-		data.std_out = dup(STDOUT_FILENO);
 		signal(SIGINT, signal_handler);
 		data.raw_cmd = display_prompt_msg();
 		if (!data.raw_cmd)
-			break; //ctrl + d
-		data.status = 0;
-		if (!data.raw_cmd)
-		{
-			if (isatty(STDIN_FILENO))
-			write(2, "exit\n", 6);
-			exit (EXIT_SUCCESS);
-		}
-		// is_redirect(data.raw_cmd);
+			ft_ctrl_d();
 		if (ft_strlen(data.raw_cmd) > 0 && ft_isstralnum(data.raw_cmd))
 			ft_commands(&data);
-		dup2(data.std_in, STDIN_FILENO);
-		dup2(data.std_out, STDOUT_FILENO);
+		reset_term_fd(&data);
 	}
-	close(data.std_in);
-	close(data.std_out);
-	restore_term(&data);
-
-	//ft_freemini(env);
+	if (finish_minishell(&data))
+		;
 	return (0);
 }
-
