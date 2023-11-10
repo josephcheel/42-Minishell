1581afe9e75e72cc1f534bde4f316ec63dfedf10
Joseph Cheel Nakashima <josephcheel@MacBook-Air-de-Joseph.local>
2023-11-10 23:51:09 +0100

    some leaks

--- src/command/commands.c
+++ src/command/commands.c
@@ -3,11 +3,33 @@
 void	ft_init_data_one_cmd(t_minishell *data)
 {
 	data->cleaned_cmd = ft_clean_redir_cmd(data->raw_cmd);
-	ft_get_redit_value(data->raw_cmd, data);
-	// printf("cleaned: %s\n raw %s", data->cleaned_cmd, data->raw_cmd);
+	
 	data->cmd = ft_split_quotes(data->cleaned_cmd);
 	data->cmd_size = ft_cmdsize(data->cmd);
 	data->cmd_and_arguments_size = ft_array_size(data->cmd);
+	// data->in_files = malloc(sizeof(t_list));
+	// data->out_files = malloc(sizeof(t_list));
+	// data->out_append = malloc(sizeof(t_list));
+	data->in_files = NULL;
+	data->out_files = NULL;
+	data->out_append = NULL;
+	data->heredocs = NULL;
+	data->is_append = 0;
+	data->is_heredoc = 0;
+	ft_get_redit_value(data->raw_cmd, data);
+}
+
+void ft_free_list(t_list **head)
+{
+	t_list *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp->content);
+		free(temp);
+	}
 }
 
 static void	ft_free_one(t_minishell *data)
@@ -18,32 +40,35 @@ static void	ft_free_one(t_minishell *data)
 		free(data->cleaned_cmd);
 	if (data->cmd)
 		ft_array_free(data->cmd, ft_array_size(data->cmd));
-	if (data->infile)
-		free(data->infile);
-	if (data->outfile)
-		free(data->outfile);
-	if (data->heredoc)
-		free(data->heredoc);
-	data->infile = NULL;
-	data->outfile = NULL;
-	data->heredoc = NULL;
-	data->in_files = NULL;
-	data->out_files = NULL;
-	data->out_append = NULL;
-	ft_lstclear(&data->out_files, free);
-	ft_lstclear(&data->out_append, free);
-	printf("free\n"); //
+	if (ft_lstsize(data->in_files) > 0)
+		ft_free_list(&data->in_files);
+	if (ft_lstsize(data->out_files) > 0)
+		ft_free_list(&data->out_files);
+	if (ft_lstsize(data->out_append) > 0)
+		ft_free_list(&data->out_append);
+	if (ft_lstsize(data->heredocs) > 0)
+		ft_free_list(&data->heredocs);
+	// printf("free\n"); //
 }
 
 
 static int ft_syntax_errors(t_minishell *data)
 {
 	if (ft_has_valid_quotes(data->raw_cmd) == 0)
+	{
+		free(data->raw_cmd);
 		return (write(2, "quote>\n", 8));
+	}
 	if (ft_check_pipe_sytax(data))
+	{
+		free(data->raw_cmd);
 		return (1);
+	}
 	if (ft_check_redir_sytax(data->raw_cmd))
+	{
+		free(data->raw_cmd);
 		return (1);
+	}
 	return (0);
 }
 
@@ -57,8 +82,12 @@ int	ft_commands(t_minishell *data)
 
 	while (ft_strchr_variable(data->raw_cmd))
 		data->raw_cmd = ft_parse_variables(data);
+
 	if (ft_isallspace(data->raw_cmd))
+	{
+ 		free(data->raw_cmd);
 		return (0);
+	}
 
 	//one and multiple commands
 	if (data->nbr_of_cmds == 1)
