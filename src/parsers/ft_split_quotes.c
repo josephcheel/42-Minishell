b894f2bb9b31219d2c9b4db8e8cdb7f40cc69602
josephcheel <josephcheelnakashima@gmail.com>
2023-11-18 03:10:56 +0100

    comments deleted

--- src/parsers/ft_split_quotes.c
+++ src/parsers/ft_split_quotes.c
@@ -43,94 +43,6 @@ static int ft_count_words(char *str)
 	return (len);
 }
 
-// static char *quotes_substr(char *str, int start, int len)
-// {
-// 	char *new_str;
-
-// 	printf("START %d LEN %d\n", start, len);
-// 	while (str[start] == ' ')
-// 		start++;
-// 	new_str = ft_substr(str, start, len - start);
-// 	return (new_str);
-// }
-
-// static char **alloc_split_quotes(int size, char *str, char **split)
-// {
-// 	int	i;
-// 	int start;
-// 	int count;
-// 	t_quote quotes;
-
-
-// 	i = 0;
-// 	start = 0;
-// 	count = 0;
-// 	quotes.dbl = 0;
-// 	quotes.simple = 0;
-
-// 	while (str[i] == ' ')
-// 			i++;
-// 	start = i;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '\"' && quotes.simple == 0)
-// 			quotes.dbl++;
-// 		if (str[i] == '\'' && quotes.dbl == 0)
-// 			quotes.simple++;
-// 		if (quotes.dbl == 2)
-// 			quotes.dbl = 0;
-// 		if (quotes.simple == 2)
-// 			quotes.simple = 0;
-// 		printf("%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
-// 		if ((str[i] == ' ' || str[i+1] == '\0')  && quotes.simple == 0 && quotes.dbl == 0)
-// 		{
-// 			if (str[i+1] == '\0')
-// 			{
-// 				while (str[i] == ' ')
-// 					i--;
-// 				printf("EMPTY NEXT\n");
-// 				split[count] = quotes_substr(str, start, i+1);
-// 			}
-// 			else if (str[i] == ' ')
-// 			{
-// 				printf("SPACE\n");
-// 				split[count] = quotes_substr(str, start, i);
-// 			}
-// 			count++;
-// 			if (size < count)
-// 				break ;
-// 			start = i;
-
-// 		}
-// 		else if ((str[i] == ' ' || str[i+1] == '\0') && (!quotes.simple && quotes.dbl || !quotes.dbl && quotes.dbl) )
-// 		{
-// 			printf("AAA%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
-// 			split[count] = quotes_substr(str, start, i);
-// 			count++;
-// 			// if (str[i+1] == '\0')
-// 			// {
-// 			// 	while (str[i] == ' ')
-// 			// 		i--;
-// 			// 	printf("EMPTY NEXT\n");
-// 			// 	split[count] = quotes_substr(str, start, i+1);
-// 			// }
-// 			// else if (str[i] == ' ')
-// 			// {
-// 			// 	printf("SPACE\n");
-// 			// 	split[count] = quotes_substr(str, start, i);
-// 			// }
-// 			// count++;
-// 			// if (size < count)
-// 			// 	break ;
-// 			// start = i;
-// 		}
-// 		while (str[i] == ' ')
-// 			i++;
-// 		i++;
-// 	}
-// 	return (split);
-// }
-
 static char **alloc_split_quotes(int size, char *str, char **split)
 {
 	int	i;
@@ -170,7 +82,6 @@ static char **alloc_split_quotes(int size, char *str, char **split)
 				split[j] = ft_substr(str, start, i- start);
 			else if (str[i+1] == '\0')
 			{
-				// printf("HOLA");
 				if (str[i] == ' ')
 					split[j] = ft_substr(str, start+1, i - start-1);
 				else
@@ -275,8 +186,6 @@ static void ft_clean_split(char **split)
 			new_str = ft_copy_word(split[i], x);
 			free(split[i]);
 			split[i] = new_str;
-			// printf("new_str %s\n", new_str);
-			// new_str[i] = ft_substr_free(split[i], 0, ft_strlen(split[i]), 0);
 		}
 		i++;
 	}
@@ -287,11 +196,8 @@ char **ft_split_quotes(char *str)
 {
 	int size;
 	char **split;
-	
-	// printf("line split: $%s$", str);
-	size = ft_count_words(str);
 
-	// printf("size %d\n", size);
+	size = ft_count_words(str);
 	split = malloc(sizeof(char *) * (size + 1));
 	if (!split)
 		return NULL;
@@ -301,8 +207,7 @@ char **ft_split_quotes(char *str)
 	return (split);
 }
 
-// NO_COLOR		=	\x1b[0m
-// OK_COLOR		=	\x1b[32;01m
+
 // int main(int ac, char **av)
 // {
 // // 	// char *str = ft_strdup("echo \"adios\"\"adios\" adios\"sfjsak\" \"echo adios bondia\" \"   \"");
