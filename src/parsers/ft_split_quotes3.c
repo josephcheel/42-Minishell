de915af109de4eb7cbc6d836a078a2b71ce3481a
josephcheel <josephcheelnakashima@gmail.com>
2023-11-18 02:00:47 +0100

    Split quotes 3. Working but when spaces insides quotes not

--- src/parsers/ft_split_quotes3.c
+++ src/parsers/ft_split_quotes3.c
@@ -33,7 +33,7 @@ static int ft_count_words(char *str)
 		if (str[i] == '\"' && quotes.dbl == 2)
 			quotes.dbl = 0;
 		if (str[i] == '\'' && quotes.simple == 2)
-			quotes.simple = str[i];
+			quotes.simple = 0;
 		if (ft_isprint(str[i]) && str[i] != ' ' && quotes.simple == 0 && quotes.dbl == 0)
 		{
 			len++;
@@ -56,111 +56,173 @@ static char *quotes_substr(char *str, int start, int len)
 	return (new_str);
 }
 
+// static char **alloc_split_quotes(int size, char *str, char **split)
+// {
+// 	int	i;
+// 	int start;
+// 	int count;
+// 	t_quote quotes;
+
+
+// 	i = 0;
+// 	start = 0;
+// 	count = 0;
+// 	quotes.dbl = 0;
+// 	quotes.simple = 0;
+
+// 	while (str[i] == ' ')
+// 			i++;
+// 	start = i;
+// 	while (str[i] != '\0')
+// 	{
+// 		if (str[i] == '\"' && quotes.simple == 0)
+// 			quotes.dbl++;
+// 		if (str[i] == '\'' && quotes.dbl == 0)
+// 			quotes.simple++;
+// 		if (quotes.dbl == 2)
+// 			quotes.dbl = 0;
+// 		if (quotes.simple == 2)
+// 			quotes.simple = 0;
+// 		printf("%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
+// 		if ((str[i] == ' ' || str[i+1] == '\0')  && quotes.simple == 0 && quotes.dbl == 0)
+// 		{
+// 			if (str[i+1] == '\0')
+// 			{
+// 				while (str[i] == ' ')
+// 					i--;
+// 				printf("EMPTY NEXT\n");
+// 				split[count] = quotes_substr(str, start, i+1);
+// 			}
+// 			else if (str[i] == ' ')
+// 			{
+// 				printf("SPACE\n");
+// 				split[count] = quotes_substr(str, start, i);
+// 			}
+// 			count++;
+// 			if (size < count)
+// 				break ;
+// 			start = i;
+
+// 		}
+// 		else if ((str[i] == ' ' || str[i+1] == '\0') && (!quotes.simple && quotes.dbl || !quotes.dbl && quotes.dbl) )
+// 		{
+// 			printf("AAA%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
+// 			split[count] = quotes_substr(str, start, i);
+// 			count++;
+// 			// if (str[i+1] == '\0')
+// 			// {
+// 			// 	while (str[i] == ' ')
+// 			// 		i--;
+// 			// 	printf("EMPTY NEXT\n");
+// 			// 	split[count] = quotes_substr(str, start, i+1);
+// 			// }
+// 			// else if (str[i] == ' ')
+// 			// {
+// 			// 	printf("SPACE\n");
+// 			// 	split[count] = quotes_substr(str, start, i);
+// 			// }
+// 			// count++;
+// 			// if (size < count)
+// 			// 	break ;
+// 			// start = i;
+// 		}
+// 		while (str[i] == ' ')
+// 			i++;
+// 		i++;
+// 	}
+// 	return (split);
+// }
+
 static char **alloc_split_quotes(int size, char *str, char **split)
 {
-	// int	i;
-	// int start;
-	// int count;
-	// t_quote quotes;
-
-
-	// i = 0;
-	// start = 0;
-	// count = 0;
-	// quotes.dbl = 0;
-	// quotes.simple = 0;
-
-	// while (str[i] == ' ')
-	// 		i++;
-	// start = i;
-	// while (str[i] != '\0')
-	// {
-	// 	if (str[i] == '\"' && quotes.simple == 0)
-	// 		quotes.dbl++;
-	// 	if (str[i] == '\'' && quotes.dbl == 0)
-	// 		quotes.simple++;
-	// 	if (quotes.dbl == 2)
-	// 		quotes.dbl = 0;
-	// 	if (quotes.simple == 2)
-	// 		quotes.simple = 0;
-	// 	printf("%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
-	// 	if ((str[i] == ' ' || str[i+1] == '\0')  && quotes.simple == 0 && quotes.dbl == 0)
-	// 	{
-	// 		if (str[i+1] == '\0')
-	// 		{
-	// 			while (str[i] == ' ')
-	// 				i--;
-	// 			printf("EMPTY NEXT\n");
-	// 			split[count] = quotes_substr(str, start, i+1);
-	// 		}
-	// 		else if (str[i] == ' ')
-	// 		{
-	// 			printf("SPACE\n");
-	// 			split[count] = quotes_substr(str, start, i);
-	// 		}
-	// 		count++;
-	// 		if (size < count)
-	// 			break ;
-	// 		start = i;
-
-	// 	}
-	// 	else if ((str[i] == ' ' || str[i+1] == '\0') && (!quotes.simple && quotes.dbl || !quotes.dbl && quotes.dbl) )
-	// 	{
-	// 		printf("AAA%s, simple %d double %d\n", &str[i], quotes.simple, quotes.dbl);
-	// 		split[count] = quotes_substr(str, start, i);
-	// 		count++;
-	// 		// if (str[i+1] == '\0')
-	// 		// {
-	// 		// 	while (str[i] == ' ')
-	// 		// 		i--;
-	// 		// 	printf("EMPTY NEXT\n");
-	// 		// 	split[count] = quotes_substr(str, start, i+1);
-	// 		// }
-	// 		// else if (str[i] == ' ')
-	// 		// {
-	// 		// 	printf("SPACE\n");
-	// 		// 	split[count] = quotes_substr(str, start, i);
-	// 		// }
-	// 		// count++;
-	// 		// if (size < count)
-	// 		// 	break ;
-	// 		// start = i;
-	// 	}
-	// 	while (str[i] == ' ')
-	// 		i++;
-	// 	i++;
-	// }
-	// return (split);
-	int i;
+	int	i;
+	int j;
 	int start;
-	int count;
+	int in_word;
 	t_quote quotes;
 
 	i = 0;
-	count = 0;
+	j = 0;
 	start = 0;
+	in_word = 0;
 	quotes.dbl = 0;
 	quotes.simple = 0;
-	while (str[i])
+	while (str[i] == ' ')
+		i++;
+	start = i;
+	while (str[i] && j < size) 
 	{
-		if (str[i] == '\"' && quotes.simple == 0)
-			quotes.dbl++;
-		if (str[i] == '\'' && quotes.dbl == 0)
-			quotes.simple++;
-		if (str[i] == '\"' && quotes.dbl == 2)
-			quotes.dbl = 0;
-		if (str[i] == '\'' && quotes.simple == 2)
-			quotes.simple = 0;
-		if ((str[i] == '\"' && !quotes.simple ) || ( str[i] == '\'' && !quotes.dbl))
-			;
-		else
+		if (ft_isprint(str[i]) && str[i] != ' ')// && quotes.simple != 1 && quotes.dbl != 1)// || quotes.simple == 1 && quotes.dbl == 1)
 		{
-			split[count] = ft_substr(str, start, i - start);
-			count++;
+			// start = i;
+			//  while ((ft_isprint(str[i]) && str[i] != ' ')||( str[i] == '\'' && !quotes.dbl)|| (str[i] == '\"' && !quotes.simple)&& str[i+1] != '\0')
+			while ((ft_isprint(str[i]) && str[i] != ' ') || ((str[i] == '\"' && !quotes.simple  || str[i] == '\''  && !quotes.dbl )) && str[i+1] != '\0')
+			{
+				// printf("SIMPLE %d, DOUBLE %d\n", quotes.simple, quotes.dbl);
+				if (str[i] == '\"' && quotes.simple == 0)
+					quotes.dbl++;
+				if (str[i] == '\'' && quotes.dbl == 0)
+					quotes.simple++;
+				// if (str[i] == '\"' && quotes.dbl == 2)
+				// 	quotes.dbl = 0;
+				// if (str[i] == '\'' && quotes.simple == 2)
+				// 	quotes.simple = 0;
+				i++;
+			}
+			printf("start %c[%d] END %c[%d]\n", str[start], start, str[i], i);
+			if ((!ft_isstrprint(&str[i+1]) && str[i] != ' ')&& (str[start] == '\'' || str[start] == '\"'))
+			{
+				printf("1 %d\n", j);
+				split[j] = ft_substr(str, start, i- start+1);
+			}
+			else if (str[i+1] == '\0' && !ft_isstrprint(&str[i+1]) && (str[start] == '\'' || str[start] == '\"'))
+			{
+				printf("2 %d\n", j);
+				printf("SIMPLE %d, DOUBLE %d\n", quotes.simple, quotes.dbl);
+				split[j] = ft_substr(str, start, i - start);
+				// else
+				// 	split[j] = ft_substr(str, start, i - start-1);
+			}
+			else if (str[i+1] == '\0')
+			{
+				printf("7 %d\n", j);
+					printf("HOLIA start %c[%d] END %c[%d]\n", str[start], start, str[i], i);
+				printf("STRING $%s$\n", &str[i]);
+				if (str[i] == ' ')
+				{
+					printf("HOLAI\n");
+					split[j] = ft_substr(str, start, i - start);
+				}
+				else
+				{
+					printf("ADEUII\n");
+					split[j] = ft_substr(str, start, i-1);
+				}
+			}
+			else if (j == 0)
+			{
+				printf("3 %d\n", j);
+				split[j] = ft_substr(str, start, i- start);
+			}
+			else
+			{
+				printf("SIMPLE %d, DOUBLE %d\n", quotes.simple, quotes.dbl);
+				printf("4 %d\n", j);
+				split[j] = ft_substr(str, start, i - start);
+			}
+			j++;
+			while (str[i] == ' ')
+				i++;
+			start = i;
+			if (str[i] == '\"' && quotes.dbl == 1)
+				quotes.dbl = 0;
+			if (str[i] == '\'' && quotes.simple == 1)
+				quotes.simple = 0;
 		}
 		i++;
 	}
 	return (split);
+
 }
 
 static int ft_size_word(char *str)
@@ -286,7 +348,7 @@ int main(int ac, char **av)
 		return 0;
 
 	printf("STRING\t\x1b[32;01m$%s$\x1b[0m\n", av[1]);
-	
+	printf("SIZE %zu\n", ft_strlen(av[1]));
 	split = ft_split_quotes(av[1]);
 	// return (0);
 	// ft_clean_split(split);
@@ -298,6 +360,8 @@ int main(int ac, char **av)
 		printf("$%s$\n", split[1]);
 	if (size >= 2)
 		printf("$%s$\n", split[2]);
+	if (size >= 3)
+		printf("$%s$\n", split[3]);
 	// printf("$%s$\n", split[3]);
 	// free(str);
 	// ft_array_free(split, ft_array_size(split));
