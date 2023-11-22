ae0e032278dae93ef4563bbba1dd626aad1550df
ageiser <ageiser@student.42barcelona.com>
2023-11-22 20:22:31 +0100

    last minute

--- src/redirections/ft_heredoc.c
+++ src/redirections/ft_heredoc.c
@@ -12,6 +12,23 @@
 
 #include "../../inc/minishell.h"
 
+
+static void	ft_remove_heredoc_file(void)
+{
+	struct stat	buffer;
+
+	if (access(HEREDOC_FILE, F_OK) != -1)
+	{
+		if (stat(HEREDOC_FILE, &buffer) == 0)
+		{
+			if (S_ISREG(buffer.st_mode))
+				unlink(HEREDOC_FILE);
+		}
+		else
+			perror ("stat");
+	}
+}
+
 static void	ft_heredoc_loop(t_list *temp, int fd)
 {
 	char	*line;
@@ -24,9 +41,13 @@ static void	ft_heredoc_loop(t_list *temp, int fd)
 		if (ft_strcmp(line, temp->content) == 0)
 		{
 			close(fd);
-			fd = open(HEREDOC_FILE, O_WRONLY | O_CREAT, 0644);
-			if (fd == -1)
-				return (perror ("open"));
+			if (temp->next)
+			{
+				ft_remove_heredoc_file();
+				fd = open(HEREDOC_FILE, O_WRONLY | O_CREAT | O_TRUNC , 0644);
+				if (fd == -1)
+					return (perror ("open"));
+			}
 			temp = temp->next;
 		}
 		else
@@ -36,6 +57,8 @@ static void	ft_heredoc_loop(t_list *temp, int fd)
 	close (fd);
 }
 
+// search for END or IN
+
 void	ft_heredoc(t_minishell *data)
 {
 	int		fd;
@@ -43,7 +66,7 @@ void	ft_heredoc(t_minishell *data)
 
 	signal(SIGINT, signal_handler_heredoc);
 	signal(SIGQUIT, signal_handler_heredoc);
-	fd = open(HEREDOC_FILE, O_WRONLY | O_CREAT, 0644);
+	fd = open(HEREDOC_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (perror ("open"));
 	temp = data->heredocs;
